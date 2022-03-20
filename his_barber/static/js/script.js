54f7020b9e2f61a2f4158216e973a4222ba64246
nickita38r <72089015+nickita38r@users.noreply.github.com>
2022-03-20 16:23:05 +0800

    podelal_hui

--- his_barber/static/js/script.js
+++ his_barber/static/js/script.js
@@ -1,6 +1,60 @@
+
 $('.block-uns-context').slick({
-  infinite: true,
+  infinite: false,
+  speed: 500,
   slidesToShow: 4,
   slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+    
+  ]
+});
+$('.block-uns-work').slick({
+  
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    
+  ]
 });
 
