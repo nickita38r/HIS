ff08a1c1db868aef0842ce7c46c1e0e7ce66e660
nickita38r <72089015+nickita38r@users.noreply.github.com>
2022-04-02 00:03:04 +0800

    111

--- his_barber/static/js/script.js
+++ his_barber/static/js/script.js
@@ -4,28 +4,31 @@ $('.block-uns-context').slick({
   speed: 500,
   slidesToShow: 4,
   slidesToScroll: 1,
+  dots: true,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 991,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 1,
         infinite: true,
-        
+        dots: true,
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 768,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
+        
       }
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1,
+        
       }
     }
     
